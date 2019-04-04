972a7bbeaa89c8e8fd94d5410bc342ca7456c51d
bcmichael <bcmichael101@yahoo.com>
2019-04-04 15:48:21 -0400

    Remove z filter
    As written the z filter was wrong.

--- pp/justcpb.pp
+++ pp/justcpb.pp
@@ -1,7 +1,6 @@
 ; ========================
 ; DEFINITIONS
 ; ========================
-allocx pls pls_z alloc_usr
 allocx pls pls_flipdn alloc_usr
 allocx pls pls_flipup alloc_usr
 
@@ -9,7 +8,6 @@ allocx ppmd ppmd_cpdly
 allocx ppmd ppmd_flipdn
 allocx ppmd ppmd_flipup
 
-allocx pwx pwx_zfilter
 allocx pwx pwx_flip
 
 ; ==========================
@@ -21,7 +19,6 @@ allocx pwx pwx_flip
 
 use timer
 use pwxpls cppls				;nam,pwr,psx,ppmd,pwx
-use pwxpls zfilter				;nam,pwr,psx,ppmd,pwx
 use pwxdly cpdly pwrh 1 ppmd_cpdly * 64		;nam,pwr,psx,ppmd,beg,size
 use pwxpls flipdn pwrh 1 ppmd_flipdn pwx_flip
 use pwxpls flipup pwrh 1 ppmd_flipup pwx_flip
@@ -30,7 +27,6 @@ iniobs
 pwxpls cppls		;dummy cp pls
 pwxdly cpdly		;psx=1, ppmd=cpdly
 pwxpls flipup
-pwxpls zfilter
 pwxpls flipdn
 sample
 recycle
@@ -50,7 +46,6 @@ use pwxpls cppls pwrh 1 ppmd_cppls * 		;nam pwr psx ppmd pwx
 use pwxdly cpdly pwrh 1 ppmd_cpdlydec * 1	;nam pwr psx ppmd beg size
 use pwxpls flipdn pwrh 1 ppmd_dec pwx_cwdec
 use pwxpls flipup pwrh 1 ppmd_dec pwx_cwdec
-use pwxpls z 1 0 pwx_zfilter
 use period sample tppm pwrh * 64 ppmd_dec
 ; <pername> <seqnam> <pwr> <org> <size> <ppmd> ;* means use the default name
 
@@ -58,7 +53,6 @@ inidec
 pwxpls cppls		;pls_cppls, psx=1, ppmd=cppls pwx 2 cppls2
 pwxdly cpdly		;constant amplitude cp, phase=y,pwx 2 cpdly2
 pwxpls flipup
-pwxpls z
 pwxpls flipdn
 sample
 recycle
