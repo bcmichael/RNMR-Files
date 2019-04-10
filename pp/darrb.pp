f57e3ba5eaf2a2063a2bc41a3171134a8feef01e
bcmichael <bcmichael101@yahoo.com>
2019-04-10 16:00:35 -0400

    Remove marks
    The marks are not needed for troubleshooting

--- pp/darrb.pp
+++ pp/darrb.pp
@@ -1,9 +1,6 @@
 ; ==========================
 ; DEFINITIONS
 ; ==========================
-
-mark 1
-
 allocx dly,dly_t1,alloc_int
 allocx dly,dly_moret1,alloc_int
 
@@ -12,8 +9,6 @@ allocx dly,dly_mix,alloc_usr
 allocx ppmd,ppmd_dec
 
 allocx pwx,pwx_flip
-
-mark 2
 
 ; =============================
 ; 2nd dimension
@@ -43,32 +38,21 @@ else
    pop
 endif
 
-mark 3
-
-
 use timer
 
 ; ==========================
 ; DEFINE OBSERVE PROGRAM
 ; ==========================
-
-mark 10
 use pwxpls,cppls
 use pwxdly,cpdly,pwrh,1,*,*,64	;nam,pwr,psx,ppmd,beg,size
 use pwxpls,flipup,pwrh,1,*,pwx_flip
 use pwxpls,flipdn,pwrh,1,*,pwx_flip
-
-mark 11
 
 iniobs
 
 ; ramped CP
 pwxpls cppls		;dummy cp pls
 pwxdly cpdly		;psx=1, ppmd=cpdly
-
-mark 12
-
-mark 13
 
 ; t1 evolution
 
@@ -78,13 +62,7 @@ pwxpls flipup
 
    dly gate_idle,dly_mix
 
-mark 14
-
-mark 15
-
 pwxpls flipdn
-
-mark 16
 
 sample
 
@@ -100,8 +78,6 @@ finobs
 ; Allocations on Decouple Channel:
 allocx pwx,pwx_cwdec
 
-mark 20
-
 use pwxpls,cppls,pwrh,1,*,*  		;nam,pwr,psx,ppmd,pwx
 use pwxdly,cpdly,pwrh,1,*,*,64	;nam,pwr,psx,ppmd,beg,size
 use pwxpls,flipup,pwrh,1,ppmd_dec,pwx_cwdec
@@ -112,17 +88,11 @@ use period,evolve,tppm,pwrh,*,64,ppmd_dec
 
 use period,mix,cw,pwrh,ppmd_cwdec
 
-mark 21
-
 inidec
 
 ; ramped CP
 pwxpls cppls		;pls_cppls,psx=1,ppmd=cppls pwx 2 cppls2
 pwxdly cpdly		;constant amplitude cp, phase=y,pwx 2 cpdly2
-
-mark 22
-
-mark 23
 
 ; t1 evolution _ dec channel
 
@@ -130,17 +100,9 @@ period evolve,dly_t1
 
 pwxpls flipup
 
-mark 24
-
       period mix,dly_mix
 
-mark 25
-
-mark 26
-
 pwxpls flipdn
-
-mark 27
 
 sample
 
