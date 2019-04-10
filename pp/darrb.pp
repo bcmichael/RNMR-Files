fff026a044c96e353b86203f7fb93976bf2fdc89
bcmichael <bcmichael101@yahoo.com>
2019-04-10 16:03:16 -0400

    Use space instead of comma as argument delimiter

--- pp/darrb.pp
+++ pp/darrb.pp
@@ -1,14 +1,14 @@
 ; ==========================
 ; DEFINITIONS
 ; ==========================
-allocx dly,dly_t1,alloc_int
-allocx dly,dly_moret1,alloc_int
+allocx dly dly_t1 alloc_int
+allocx dly dly_moret1 alloc_int
 
-allocx dly,dly_mix,alloc_usr
+allocx dly dly_mix alloc_usr
 
-allocx ppmd,ppmd_dec
+allocx ppmd ppmd_dec
 
-allocx pwx,pwx_flip
+allocx pwx pwx_flip
 
 ; =============================
 ; 2nd dimension
@@ -17,23 +17,23 @@ use dim 2 1 2
 ; idim,logical_chn,nseg
 
 dimdly 2
-pop dly,dly_t1
+pop dly dly_t1
 
 ; ==========================
 ; CALCULATIONS
 ; ==========================
 
 ; moret1 = size*step-t1
-push loop,loop_size$2
-push dly,dly_step$2
+push loop loop_size$2
+push dly dly_step$2
 mul
-push dly,dly_t1
+push dly dly_t1
 ifstk lt
   sub
-  pop dly,dly_moret1
+  pop dly dly_moret1
 else
-   push dly,#0
-   pop dly,dly_moret1
+   push dly #0
+   pop dly dly_moret1
    pop
    pop
 endif
@@ -43,10 +43,10 @@ use timer
 ; ==========================
 ; DEFINE OBSERVE PROGRAM
 ; ==========================
-use pwxpls,cppls
-use pwxdly,cpdly,pwrh,1,*,*,64	;nam,pwr,psx,ppmd,beg,size
-use pwxpls,flipup,pwrh,1,*,pwx_flip
-use pwxpls,flipdn,pwrh,1,*,pwx_flip
+use pwxpls cppls
+use pwxdly cpdly pwrh 1 * * 64	;nam,pwr,psx,ppmd,beg,size
+use pwxpls flipup pwrh 1 * pwx_flip
+use pwxpls flipdn pwrh 1 * pwx_flip
 
 iniobs
 
@@ -56,17 +56,17 @@ pwxdly cpdly		;psx=1, ppmd=cpdly
 
 ; t1 evolution
 
-	dly gate_idle,dly_t1
+	dly gate_idle dly_t1
 
 pwxpls flipup
 
-   dly gate_idle,dly_mix
+   dly gate_idle dly_mix
 
 pwxpls flipdn
 
 sample
 
-dly gate_idle,dly_moret1
+dly gate_idle dly_moret1
 
 recycle
 finobs
@@ -76,17 +76,17 @@ finobs
 ; ==========================
 
 ; Allocations on Decouple Channel:
-allocx pwx,pwx_cwdec
+allocx pwx pwx_cwdec
 
-use pwxpls,cppls,pwrh,1,*,*  		;nam,pwr,psx,ppmd,pwx
-use pwxdly,cpdly,pwrh,1,*,*,64	;nam,pwr,psx,ppmd,beg,size
-use pwxpls,flipup,pwrh,1,ppmd_dec,pwx_cwdec
-use pwxpls,flipdn,pwrh,1,ppmd_dec,pwx_cwdec
-use period,sample,tppm,pwrh,*,64,ppmd_dec
-use period,evolve,tppm,pwrh,*,64,ppmd_dec
+use pwxpls cppls pwrh 1 * *  		;nam,pwr,psx,ppmd,pwx
+use pwxdly cpdly pwrh 1 * * 64	;nam,pwr,psx,ppmd,beg,size
+use pwxpls flipup pwrh 1 ppmd_dec pwx_cwdec
+use pwxpls flipdn pwrh 1 ppmd_dec pwx_cwdec
+use period sample tppm pwrh * 64 ppmd_dec
+use period evolve tppm pwrh * 64 ppmd_dec
 ;pernam,seqnam,pwr,org,size,ppmd
 
-use period,mix,cw,pwrh,ppmd_cwdec
+use period mix cw pwrh ppmd_cwdec
 
 inidec
 
@@ -96,17 +96,17 @@ pwxdly cpdly		;constant amplitude cp, phase=y,pwx 2 cpdly2
 
 ; t1 evolution _ dec channel
 
-period evolve,dly_t1
+period evolve dly_t1
 
 pwxpls flipup
 
-      period mix,dly_mix
+      period mix dly_mix
 
 pwxpls flipdn
 
 sample
 
-period evolve,dly_moret1
+period evolve dly_moret1
 
 recycle
 findec
